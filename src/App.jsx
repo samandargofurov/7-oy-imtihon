3f62dbb74a9ac4332b31cef635d4aefbac0f6332
samandargofurov <samandargofurov236@gmail.com>
2024-04-30 10:34:24 +0500

    exam

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-
+  useEffect(() => {
+  },[])
   return (
     <>
-      
+      <h1></h1>
+      <img src='' alt="" />
     </>
   )
 }
